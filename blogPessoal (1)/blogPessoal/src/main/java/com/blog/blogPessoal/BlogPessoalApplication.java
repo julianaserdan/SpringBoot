3b3076be71b5f4aad685ef6e60e7fab837f63bba
julianaserdan <juliana.santos.a@hotmail.com>
2021-01-27 13:18:14 -0300

    Atividade SpringBoot

---
+++ blogPessoal (1)/blogPessoal/src/main/java/com/blog/blogPessoal/BlogPessoalApplication.java
@@ -0,0 +1,13 @@
+package com.blog.blogPessoal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogPessoalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogPessoalApplication.class, args);
+	}
+
+}
